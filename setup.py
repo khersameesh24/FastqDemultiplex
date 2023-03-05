3e043dd4d4c1a7325c2974330375c0f3bf7fa6f1
khersameesh24 <khersameesh24@gmail.com>
2023-03-05 14:25:59 +0000

    initial commit, initialize project(package) directory structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='demult',
+    description="A command line utility to demultiplex fastq files.",
+    version='1.0',
+    scripts=['demult'],
+    author='Sameesh Kher',
+    author_email="khersameesh24@gmail.com",
+    maintainer='Sameesh Kher',
+    maintainer_email="khersameesh24@gmail.com",
+    packages=[],
+    python_requires='>=3.5',
+    install_requires=[]
+)
